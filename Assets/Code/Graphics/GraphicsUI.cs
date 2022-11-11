2435fca882eaa72aa5d784afe3a1c50a7e6ada87
Matthias Artmann <martmann@mail.uni-paderborn.de>
2022-11-11 12:08:09 +0100

    Updated template and added class in global namespace

---
+++ Assets/Code/Graphics/GraphicsUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is a class that is not contained in any namespace.
+/// It will be displayed under the Global namespace.
+/// <para>
+/// It also inherits lots of members from MonoBehaviour.
+/// </para>
+/// </summary>
+public class GraphicsUI : MonoBehaviour
+{
+    
+}
